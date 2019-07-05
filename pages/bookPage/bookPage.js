d90201fb13b85b229dc1ead3959e7386235cb804
HLt <lichangcheng003@ke.com>
2019-07-05 11:39:38 +0800

    write:预订页面添加日期、场次、套餐、数量

--- pages/bookPage/bookPage.js
+++ pages/bookPage/bookPage.js
@@ -5,9 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    comboList: [
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午1点',
+        'price': '250',
+      },
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午2点',
+        'price': '250',
+      },
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午3点',
+        'price': '250',
+      },
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午4点',
+        'price': '250',
+      },
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午5点',
+        'price': '250',
+      },
+      {
+        'name': '普通门票 + 全岛接送',
+        'date': '下午6点',
+        'price': '250',
+      }
+    ],
+    isChooseIndex: 0,
+    isSeesionIndex: 0
   },
-
+  // 选择套餐
+  chooseCombo(event) {
+    const newIndex = event.target.dataset.comboindex
+    if (newIndex == this.data.isChooseIndex) {
+      return
+    }
+    this.setData({
+      isChooseIndex: newIndex || 0
+    })
+  },
+  // 选择场次
+  chooseSession(event) {
+    const newIndex = event.target.dataset.seesionindex
+    if (newIndex == this.data.isSeesionIndex) {
+      return
+    }
+    this.setData({
+      isSeesionIndex: newIndex || 0
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
